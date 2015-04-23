e9a6fca2a52c47fa528d162618adab125ac18680
白玄 <zhili.wzl@alibaba-inc.com>
2015-04-24 01:32:10 +0800

    update readme

--- README.md
+++ README.md
@@ -174,377 +174,10 @@ KVM.isValue 判断是否是值类型
 KVM.isEmpty 判断对象是否为空，可以判断数组也可以判断对象
 
 
-KVM.guid 获取一个唯一的guid值
+###内置模块接口
 
-
-KVM.unique 对数组去重
-
-
-KVM.toArray 转换为数组
-
-
-KVM.bind 函数上下文绑定
-
-```
-var obj = {
-    name:"hello world"
-};
-function fun(param){
-    console.log(param+this.name);
-}
-kvm.bind(fun,obj)("janry,");
-
-//output janry,hello world
-
-```
-
-KVM.forEach 遍历，可以遍历数组，也可以遍历对象
-
-```
-kvm.forEach(arr,function(item,key){
-
-},function(){//如果arr的类型不是数组或者数组长度为0会触发该回调
-
-})
-
-```
-
-KVM.merge 合并对象，类似jquery的extend,支持深度合并也支持浅合并
-
-```
-kvm.merge([isDeep/*是否深度合并*/],obj1,obj2,...,[isCover/*是否覆盖属性*/],[callback/*回调，param:name,target,source*/])
-
-```
-
-
-KVM.copy 复制对象，可以通过回调动态的设置哪些属性浅拷贝，哪些属性深拷贝
-
-```
-kvm.copy(isDeep/*可选*/,source/*源对象*/,callback/*筛选回调，参数：item,key,path*/)
-
-如果 callback 返回true，而且设置了isDeep为true，则对于某个属性是深度拷贝，否则是浅拷贝
-```
-
-#默认模块
-
-####$do 一个json对象的操作工具，通过传入类似dsl语法命令对象来操作数据源
-
-####API接口
-
-1.$do作为构造函数
-
-```
-
-    new $do(data/*数据源*/,cmds/*命令对象*/);这样可以直接对对象中的数据进行各种操作
-    
-    or 
-    
-    var instance = new $do(data);
-    instance.exec(cmds);
-    
-```
-
-2.$do.exec 静态方法
-
-```
-   $do.exec(data,cmds,hook/*拥有输出的命令中所预留的回调*/);
-   
-```
-
-支持的命令有：
-
-1.直接操作对象引用的命令
- * $remove 删除对象中的某一元素
- 
- ```
-    var source = {
-        name:"janry"
-    }
-    var cmds = {
-        $remove:{
-            name:true
-        }
-    }
-    $do.exec(source,cmds);//删除name
- ```
- 
- * $set 给对象某一属性设置值
- 
- ```
-     var source = {}
-     var cmds = {
-         $set:{
-            name:"janry"
-         }
-     }
-     $do.exec(source,cmds);//添加name
-     
- ```
- 
- * $push 给对象中的某个数组push一个值
- 
-  ```
-      var source = {
-         aa:[]
-      }
-      var cmds = {
-         aa:{
-            $push:{
-                name:"janry",
-                age:23
-            }
-         }
-      }
-      $do.exec(source,cmds);//给aa添加一个对象
-      
-  ```
-  
- * $slice 将对象中的某个数组截取为某个子数组，注意，数组对象的操作都是直接对数据源的引用进行操作
- 
- ```
-       var source = {
-          aa:[1,2,3,4,5]
-       }
-       var cmds = {
-          aa:{
-             $slice:[0,2]
-          }
-       }
-       $do.exec(source,cmds);
-       //output source = {aa:[1,2]};
-      
- ```
- 
- * $concat 将对象中的某个数组与新数组合并
-  
- ```
-       var source = {
-          aa:[1,2,3,4,5]
-       }
-       var cmds = {
-          aa:{
-             $concat:[122,34]
-          }
-       }
-       $do.exec(source,cmds);
-       //output source = {aa:[1,2,3,4,5,122,34]};
-      
- ```
-   
- * $pop 弹出对象中某个数组的最后一个元素
- 
- ```
-        var source = {
-           aa:[1,2,3,4,5]
-        }
-        var cmds = {
-           aa:{
-              $pop:true
-           }
-        }
-        $do.exec(source,cmds);
-        //output source = {aa:[1,2,3,4]};
-        
- ```
- 
- * $unshift 给对象中某个数组的头部插入一个值
- 
- ```
-        var source = {
-           aa:[1,2,3,4,5]
-        }
-        var cmds = {
-           aa:{
-              $unshift:2333
-           }
-        }
-        $do.exec(source,cmds);
-        //output source = {aa:[233,,2,3,4,5]};
-        
- ```
- 
- * $sort 给对象中的某个数组进行排序
- 
- 
- ```
-        var source = {
-           aa:[{name:"janry",age:23},{name:"judicy",age:20}]
-        }
-        var cmds = {
-           aa:{
-              $sort:{
-                age:"$desc"//按照age降序 $asc升序
-              }
-           }
-        }
-        $do.exec(source,cmds);
-        
- ```
- 
- * $merge 将对象中某个对象与一个新对象进行合并
-
- 
- ```
-        var source = {
-           aa:{
-              name:"janry"
-           }
-        }
-        var cmds = {
-           aa:{
-              $merge:{
-                age:23
-              }
-           }
-        }
-        $do.exec(source,cmds);
-        
- ```
- 
-
- * $deep_merge 将对象中某个对象与一个新对象进行深度合并
- 
- 
- ```
-        var source = {
-           aa:{
-              name:"janry"
-           }
-        }
-        var cmds = {
-           aa:{
-              $deep_merge:{
-                age:23,
-                home:{
-                    city:"",
-                    street:"",
-                    call:""
-                }
-              }
-           }
-        }
-        $do.exec(source,cmds);
-        
- ```
- 
- 
-2.存在输出的命令
- * $clone 拷贝某个对象
-  
-   $clone还存在子命令：$deep,$white_list,$black_list,$filter,$callback
-  
- ```
-        var source = {
-           aa:{
-              name:"janry"
-           }
-        }
-        var cmds = {
-          $clone:{
-            $deep:true,//深度克隆
-            $white_list:[],//深度克隆白名单，名单中的数据为某属性的路径，可以是a.a.a[3]这样的
-            $black_list:[],//浅克隆白名单，如果两个名单冲突，黑名单优先级大
-            $filter:function(val/*属性值*/,key/*属性名*/,path/*属性所在路径*/){
-                //返回true代表深度clone,返回false代表浅clone
-            },
-            $callback:function(cp){//克隆成功后的回调，该回调恰好在静态方法传参的hook调用后执行
-            
-            }
-          }
-        }
-        //对于clone,如果把$deep:false则没有深浅一说
-        $do.exec(source,cmds);
-        
- ```
- 
- 
- * $find 搜索对象中某个数组中的目标对象
- 
-   $find 也存在子命令：
-   
-   1.$gt 筛选集合中某一属性值字典顺序大于某指定字符串的值的对象
-   
-   2.$lt 筛选集合中某一属性值字典顺序小于某指定字符串的值的对象
-   
-   3.$is 筛选集合中某一属性值字典顺序等于某指定字符串的值的对象
-   
-   4.$not 筛选集合中某一属性值字典顺序不等于某指定字符串的值的对象
-   
-   5.$gte 筛选集合中某一属性值字典顺序大于等于某指定字符串的值的对象
-   
-   6.$lte 筛选集合中某一属性值字典顺序小于等于某指定字符串的值的对象
-   
-   7.$icontains 筛选集合中某一属性值不包含指定字符串的对象
-   
-   8.$contains 筛选集合中某一属性值包含指定字符串的对象
-   
-   9.$in 筛选集合中某一属性值存在于某一字符串数组的对象
-   
-   10.$not_in 筛选集合中某一属性值不存在于某一字符串数组的对象
-   
-   11.$and,$or 对于查询条件的逻辑操作符，可以用在与$is,$lt...等命令同级，也可以用在命令的内部级别
-   
-```
-         var source = {
-                   aa:[{name:"janry",age:23},{name:"judicy",age:20}]
-         }
-         var cmds = {
-           aa:{
-              $find:{//代表查询name等于janry或age不等于20的对象集合
-                $is:{
-                    name:"janry"
-                },
-                $or:true,
-                $not:{
-                    age:"20"
-                }
-              }
-           }
-         }
-         $do.exec(source,cmds);
-
-```
- 
- * $foreach 遍历对象中的某个指定对象
- 
-```
-         var source = {
-                   aa:[{name:"janry",age:23},{name:"judicy",age:20}]
-         }
-         var cmds = {
-           aa:{
-              $foreach:function(val,key){
-                .....
-              }
-           }
-         }
-         $do.exec(source,cmds);
-
-```
-
-####$class 类构造器
-
-####API接口
-
-```
-
-创建一个类 var Person = $class({
-    原型属性或方法
-},{
-    静态属性或方法
-});
-
-$class.inherit(subClass/*子类*/，parantClass/*父类*/,..../*支持多继承*/);//这里的继承拥有重写的功能，子类方法属性会重写父类的方法属性
-
-对于实例对象可以通过this.$parant来访问其父对象，$parent是一个数组，上面保存着多个父类实例对象，构造函数中需要通过调用this.$super()来调用父类构造函数
-
-$class.protocol({..methods..});//接口协议定义，该函数返回一个接口类，如果某类继承它的话，子类的方法必须重写该接口类的所有方法，否则会报错
-
-```
 
 ####$emitter 事件分发器
-
-####API接口
 
 ```
 var event = new $emitter(cache/*外部缓存事件队列*/);
@@ -555,6 +188,33 @@ event.$emit("eventName",param1,param2,....) 触发事件
 
 event.$remove("eventName",callback) 删除事件
 
+event.$one("eventName",callback) 注册事件，对于一个事件名只能注册一个事件处理器
 
+```
+
+###插件接口
+
+```
+
+kvm.module.registerPlugin(function(interface){
+    ....
+});
+
+这是相关暴露出来的插件接口
+
+let PluginInterface = {
+	registerModuleParser: Module.registerModuleParser,//用于模块解析，参造commonjs插件
+
+	registerDriverLoader: Driver.registerDriverLoader,//用于各种加载驱动程序
+	registerDriverLoaded: Driver.registerDriverLoaded,//驱动加载后的回调
+	registerDriverBeforeLoad: Driver.registerDriverBeforeLoad,//驱动加载前的回调
+
+	registerFileExtParser: Path.registerFileExtParser,//文件后缀解析器
+	registerPathParser: Path.registerPathParser,//路径解析器
+	registerPathMaper: Path.registerPathMaper,//路径映射解析器
+
+	createModule: Module.createModule,//创建一个模块
+	createPath: Path.createPath//创建一个path
+};
 
 ```
---
+++ dist/kvm-plugins.js
@@ -0,0 +1,383 @@
+/**
+ * 可以使kvm支持别名机制
+ */
+(function(){
+	kvm.module.registerPlugin(function(API){
+		API.registerPathMaper(function(){
+			var alias = kvm.module.data('alias');
+			if(kvm.isAbsolutePath(this.id)) return;
+			if(alias[this.id]){
+				this.uri = alias[this.id];
+				this._parser();
+			}
+		});
+	});
+})();
+
+/**
+ * 可以使kvm支持commonjs规范
+ */
+
+(function(){
+
+	kvm.module.registerPlugin(function(API){
+		API.registerModuleParser(function(){
+			function uniquePath(paths){
+				var i,j;
+				for(i=0;i < paths.length;i++){
+					for(j=i+1;j < paths.length;j++){
+						if(paths[i].equal(paths[j])){
+							paths.splice(j,1);
+						}
+					}
+				}
+				return paths;
+			}
+			this.injectCommonjs = function(){
+				var _this = this;
+				this.injectors.exports = function () {
+					_this.module.exports = _this.module.exports || {};
+					return _this.module.exports;
+				};
+				this.injectors.module = function(){
+					return _this.module;
+				};
+				this.injectors.require = function(){
+					return function(id){
+						var path = API.createPath(id);
+						var module = path.getModule();
+						return module && module.module && module.module.exports ? module.module.exports : {};
+					}
+				};
+			};
+			this.collectDeps = function(){
+				var deps;
+				if(kvm.isFunction(this.factory)) {
+					deps = this.parseDependencies(this.factory.toString());
+					deps = deps.map(function(id){
+						return API.createPath(id);
+					});
+					this.depPaths = uniquePath(this.depPaths.concat(deps));
+				}
+			};
+			this.parseDependencies = function (s) {
+				if(s.indexOf('require') == -1) {
+					return [];
+				}
+				var index = 0, peek, length = s.length, isReg = 1, modName = 0, parentheseState = 0, parentheseStack = [], res = [];
+				while(index < length) {
+					readch();
+					if(isBlank()) {
+					}
+					else if(isQuote()) {
+						dealQuote();
+						isReg = 1
+					}
+					else if(peek == '/') {
+						readch();
+						if(peek == '/') {
+							index = s.indexOf('\n', index);
+							if(index == -1) {
+								index = s.length;
+							}
+						}
+						else if(peek == '*') {
+							index = s.indexOf('*/', index);
+							if(index == -1) {
+								index = length;
+							}
+							else {
+								index += 2;
+							}
+						}
+						else if(isReg) {
+							dealReg();
+							isReg = 0
+						}
+						else {
+							index--;
+							isReg = 1;
+						}
+					}
+					else if(isWord()) {
+						dealWord();
+					}
+					else if(isNumber()) {
+						dealNumber();
+					}
+					else if(peek == '(') {
+						parentheseStack.push(parentheseState);
+						isReg = 1
+					}
+					else if(peek == ')') {
+						isReg = parentheseStack.pop();
+					}
+					else {
+						isReg = peek != ']';
+						modName = 0
+					}
+				}
+				return res;
+				function readch() {
+					peek = s.charAt(index++);
+				}
+				function isBlank() {
+					return /\s/.test(peek);
+				}
+				function isQuote() {
+					return peek == '"' || peek == "'";
+				}
+				function dealQuote() {
+					var start = index;
+					var c = peek;
+					var end = s.indexOf(c, start);
+					if(end == -1) {
+						index = length;
+					}
+					else if(s.charAt(end - 1) != '\\') {
+						index = end + 1;
+					}
+					else {
+						while(index < length) {
+							readch();
+							if(peek == '\\') {
+								index++;
+							}
+							else if(peek == c) {
+								break;
+							}
+						}
+					}
+					if(modName) {
+						res.push(s.slice(start, index - 1));
+						modName = 0;
+					}
+				}
+				function dealReg() {
+					index--;
+					while(index < length) {
+						readch();
+						if(peek == '\\') {
+							index++
+						}
+						else if(peek == '/') {
+							break
+						}
+						else if(peek == '[') {
+							while(index < length) {
+								readch();
+								if(peek == '\\') {
+									index++;
+								}
+								else if(peek == ']') {
+									break;
+								}
+							}
+						}
+					}
+				}
+				function isWord() {
+					return /[a-z_$]/i.test(peek);
+				}
+				function dealWord() {
+					var s2 = s.slice(index - 1);
+					var r = /^[\w$]+/.exec(s2)[0];
+					parentheseState = {
+						'if': 1,
+						'for': 1,
+						'while': 1,
+						'with': 1
+					}[r];
+					isReg = {
+						'break': 1,
+						'case': 1,
+						'continue': 1,
+						'debugger': 1,
+						'delete': 1,
+						'do': 1,
+						'else': 1,
+						'false': 1,
+						'if': 1,
+						'in': 1,
+						'instanceof': 1,
+						'return': 1,
+						'typeof': 1,
+						'void': 1
+					}[r];
+					modName = /^require\s*\(\s*(['"]).+?\1\s*\)/.test(s2);
+					if(modName) {
+						r = /^require\s*\(\s*['"]/.exec(s2)[0];
+						index += r.length - 2;
+					}
+					else {
+						index += /^[\w$]+(?:\s*\.\s*[\w$]+)*/.exec(s2)[0].length - 1;
+					}
+				}
+				function isNumber() {
+					return /\d/.test(peek)
+						|| peek == '.' && /\d/.test(s.charAt(index));
+				}
+				function dealNumber() {
+					var s2 = s.slice(index - 1);
+					var r;
+					if(peek == '.') {
+						r = /^\.\d+(?:E[+-]?\d*)?\s*/i.exec(s2)[0];
+					}
+					else if(/^0x[\da-f]*/i.test(s2)) {
+						r = /^0x[\da-f]*\s*/i.exec(s2)[0];
+					}
+					else {
+						r = /^\d+\.?\d*(?:E[+-]?\d*)?\s*/i.exec(s2)[0];
+					}
+					index += r.length - 1;
+					isReg = 0;
+				}
+			};
+			function needParse(depPaths){
+				var result = false;
+				depPaths.forEach(function(path){
+					if(path.id == "require"){
+						result = true;
+						return false;
+					}
+				});
+				return result;
+			}
+			this.injectCommonjs();
+			if(this.factory && needParse(this.depPaths)) {
+				this.collectDeps();
+			}
+		});
+	});
+})();
+
+/**
+ * 可以使kvm加载css文件
+ */
+(function(){
+	kvm.module.registerPlugin(function(API){
+		API.registerDriverLoader('css',function(uri,callback){
+			var doc = document;
+			var head = doc.head || doc.getElementsByTagName("head")[0] || doc.documentElement;
+			var ss = window.document.createElement( "link" );
+			var sheets = window.document.styleSheets;
+			var _this = this;
+			ss.rel = "stylesheet";
+			ss.href = uri;
+			ss.media = "only x";
+			if( callback ) {
+				ss.onload = function(){
+					callback && callback(null,ss);
+				};
+
+				ss.onerror = function(){
+					callback && callback(true);
+				};
+			}
+			head.appendChild(ss);
+			ss.onloadcssdefined = function( cb ){
+				var defined;
+				for( var i = 0; i < sheets.length; i++ ){
+					if( sheets[ i ].href && sheets[ i ].href.indexOf( uri ) > -1 ){
+						defined = true;
+					}
+				}
+				if( defined ){
+					cb();
+				} else {
+					setTimeout(function() {
+						ss.onloadcssdefined( cb );
+					});
+				}
+			};
+			ss.onloadcssdefined(function() {
+				ss.media = _this.path.query && _this.path.query.media || "all";
+			});
+		});
+
+		API.registerDriverLoaded(function (err,res) {
+			var path = this.path;
+			if(path.ext == "css" && !err){
+				kvm.module.define(path.id, function(){
+					return res;
+				});
+			}
+		});
+	});
+})();
+
+/**
+ * 可以使kvm加载json文件
+ */
+/**
+ * 可以使kvm支持包管理
+ */
+(function () {
+	kvm.module.registerPlugin(function (API) {
+		var Data = kvm.module.data();
+		API.registerPathParser(function () {
+			var baseUrl = Data.baseUrl;
+			var packages = Data.packages;
+			var packname, index,
+				uri = this.uri || this.id;
+			if(!uri) return;
+			index = uri.indexOf("/");
+			packname = uri.substr(0, index);
+			if (packages && packages[packname]) {
+				this.baseUrl = packages[packname].uri || packages[packname].url || baseUrl;
+				this.uri = uri.substr(index + 1);
+				this._parseVars();
+			}
+		});
+	});
+})();
+
+
+/**
+ * 可以使kvm支持包装器
+ */
+(function () {
+	kvm.module.registerPlugin(function (API) {
+		var Data = kvm.module.data();
+		API.registerPathMaper(function () {
+			var shim = Data.shims[this.id];
+			if (shim) {
+				this.uri = shim.uri || shim.url;
+				this._parser();
+			}
+		});
+
+		API.registerDriverBeforeLoad(function () {
+			var path = this.path;
+			var shim = Data.shims[path.id];
+			if (shim) {
+				if (shim.exports && !shim.factory) {
+					if (window[shim.exports]) {
+						this.module = API.createModule(path.id, function () {
+							return window[shim.exports];
+						});
+					}
+				}
+			}
+		});
+
+		API.registerDriverLoaded(function () {
+			var path = this.path;
+			var shim = Data.shims[path.id];
+			if (shim && !path.getModule()) {
+				if (kvm.isFunction(shim.factory)) {
+					kvm.module.define(path.id, shim.factory);
+				} else {
+					kvm.module.define(path.id, function () {
+						return window[shim.exports]
+					});
+				}
+			}
+		});
+	});
+})();
+
+
+/**
+ * 可以使kvm加载html模板
+ */
